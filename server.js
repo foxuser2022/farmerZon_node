bf1050770d8337ca87766b410ba767c26a4be766
ishwar.k <ishwar.k@globtier.in>
2025-06-06 11:23:44 +0530

    farmerzon environment

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 const PORT = process.env.PORT;
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 
 // Connect Database
 connectDB();
 
-app.use(cors({
-    origin: "*"
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/api/auth', require('./routes/auth.router'));
-app.use('/api/users', require('./routes/users.router'));
-app.use('/api/properties', require('./routes/property.router'));
+app.use("/api/auth", require("./routes/auth.router"));
+app.use("/api/users", require("./routes/users.router"));
 
 app.listen(PORT, () => {
-    console.log(`DBRhomz Api http://localhost:${PORT}`);
+  console.log(`Farmerzon Api:  http://localhost:${PORT}`);
 });
