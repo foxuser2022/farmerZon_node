437bede4976bafaca5385458a74882f8d5802f1d
ishwar.k <ishwar.k@globtier.in>
2025-06-20 16:39:12 +0530

    unused file removed

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,8 +1,7 @@
 import express, { Router } from "express";
-import { register, login } from "../controllers/auth.controller";
+const router: Router = express.Router();
 import { addProduct, buyProductList, getCategories, getSellerProducts } from "../controllers/product.controller";
 import { verifySeller } from "../middleware/seller.middleware";
-const router: Router = express.Router();
 
 router.post("/", verifySeller, addProduct);
 router.get("/my-products", verifySeller, getSellerProducts);
