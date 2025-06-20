e71583b185dad90f66e2d55e80bb524e89f8fd00
ishwar.k <ishwar.k@globtier.in>
2025-06-20 13:16:35 +0530

    product schema updated

--- src/models/Product.schema.ts
+++ src/models/Product.schema.ts
@@ -1,21 +1,52 @@
 import mongoose, { Model, Schema, Document } from "mongoose";
 
+export enum Unit {
+  KG = "kg",
+  LITRE = "litre",
+  PIECE = "piece",
+  GRAM = "gram",
+  QUINTAL = "quintal",
+  TON = "ton",
+}
+
+export interface ILocation {
+  city: string;
+  state: string;
+  geolocation: {
+    lat: number;
+    lng: number;
+  };
+  address: string;
+}
+
 export interface IProduct extends Document {
   name: string;
   description: string;
   price: number;
   quantity: number;
+  unit: Unit;
+  location: ILocation;
   category: mongoose.Schema.Types.ObjectId;
   image?: string;
   seller: mongoose.Schema.Types.ObjectId;
 }
 
-const productSchema = new Schema<IProduct>(
+const productSchema = new Schema(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true },
+    unit: { type: String, enum: Object.values(Unit), required: true },
+    location: {
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      address: { type: String, required: true },
+      geolocation: {
+        lat: { type: String, required: true },
+        lng: { type: String, required: true },
+      },
+    },
     category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
     image: {
       type: String,
--- src/models/Users.schema.ts
+++ src/models/Users.schema.ts
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema<IUser>(
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
+        Phone: { type: Number, required: true },
         role: {
             type: String,
             enum: ['seller', 'buyer'],
